366d80f5a35b899ee35e55d6c38247a7e3c91a1c
Fushko <fushkodev@outlook.it>
2019-12-03 13:09:36 +0100

    Delete

--- disable.js
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (C) 2019 Francesco Fusco. All rights reserved.
- * License: https://github.com/Fushko/font-contrast#license
- */
-
-x.removeChild(t);
-chrome.runtime.sendMessage({from: "yo", t: false});
